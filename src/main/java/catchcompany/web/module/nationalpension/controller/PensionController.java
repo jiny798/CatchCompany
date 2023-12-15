bbcd3c46e09d4d0e1b7aa666e1f00733ca651482
jyoung7988 <jyoung7988@naver.com>
2023-12-15 23:53:07 +0900

    국민연금 포트폴리오 템플릿

--- src/main/java/catchcompany/web/module/nationalpension/controller/PensionController.java
+++ src/main/java/catchcompany/web/module/nationalpension/controller/PensionController.java
@@ -1,15 +1,39 @@
 package catchcompany.web.module.nationalpension.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import catchcompany.web.module.nationalpension.controller.dto.InvestYearInfo;
+import catchcompany.web.module.nationalpension.service.PensionService;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/national-pension")
+@RequestMapping("/pension")
 public class PensionController {
 
-	
+	private final PensionService pensionService;
 
+	@GetMapping("/portfolio")
+	public String processInvestInfoSave() {
+
+		return "pension/pension_portfolio";
+	}
+
+	@ModelAttribute("yearList")
+	public List<Integer> yearList(){
+		List<Integer> yearList = new ArrayList<>();
+		yearList.add(2022);
+		yearList.add(2021);
+		yearList.add(2020);
+		yearList.add(2019);
+		yearList.add(2018);
+		return yearList;
+	}
 }
---
+++ src/main/java/catchcompany/web/module/nationalpension/service/PensionService.java
@@ -0,0 +1,7 @@
+package catchcompany.web.module.nationalpension.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PensionService {
+}
