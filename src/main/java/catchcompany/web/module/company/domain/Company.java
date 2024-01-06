5b37543825d6e25d1283182834d2ff3f69467df4
jyoung7988 <jyoung7988@naver.com>
2024-01-07 01:12:32 +0900

    feat: Company 도메인 추가

---
+++ src/main/java/catchcompany/web/module/company/domain/Company.java
@@ -0,0 +1,75 @@
+package catchcompany.web.module.company.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import catchcompany.web.module.company.infrastructure.dto.CompanyDataDto;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Company {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "company_registration_code", unique = true)
+	private String registrationCode;
+
+	@Column(name = "streetName_address")
+	private String streetNameAddress;
+
+	@Column(name = "street_number_address")
+	private String streetNumberAddress;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "business_type")
+	private String businessType;
+
+	@Column(name = "member_count")
+	private String memberCount;
+
+	@Column(name = "new_member_count")
+	private String newMemberCount;
+
+	@Column(name = "exit_member_count")
+	private String exitMemberCount;
+
+	@Column(name = "date")
+	private String date;
+
+	public static Company from(CompanyDataDto dto) {
+		Company company = Company.builder()
+			.registrationCode(dto.getCompanyRegistrationCode())
+			.streetNameAddress(dto.getStreetNameAddress())
+			.streetNumberAddress(dto.getStreetNumberAddress())
+			.name(dto.getName())
+			.businessType(dto.getBusinessType())
+			.memberCount(dto.getMemberCount())
+			.newMemberCount(dto.getNewMemberCount())
+			.exitMemberCount(dto.getExitMemberCount())
+			.date(dto.getDate())
+			.build();
+
+		return company;
+	}
+}
---
+++ src/main/java/catchcompany/web/module/company/infrastructure/dto/CompanyDataDto.java
@@ -0,0 +1,38 @@
+package catchcompany.web.module.company.infrastructure.dto;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+
+@Getter
+public class CompanyDataDto {
+	@JsonProperty("가입자수")
+	private String memberCount;
+
+	@JsonProperty("사업자등록번호")
+	private String companyRegistrationCode;
+
+	@JsonProperty("사업장도로명상세주소")
+	private String streetNameAddress;
+
+	@JsonProperty("사업장지번상세주소")
+	private String streetNumberAddress;
+
+	@JsonProperty("사업장명")
+	private String name;
+
+	@JsonProperty("사업장업종코드명")
+	private String businessType;
+
+	@JsonProperty("신규취득자수")
+	private String newMemberCount;
+
+	@JsonProperty("상실가입자수")
+	private String exitMemberCount;
+
+	@JsonProperty("자료생성년월")
+	private String date;
+
+
+}
