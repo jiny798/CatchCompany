3e447322466da44c8687df7fb2f1a4c4fd8cd3a9
jyoung7988 <jyoung7988@naver.com>
2024-01-15 00:49:14 +0900

    Test: 예외 사항이 발생할 수 있는 케이스를 추가한다

--- src/test/java/catchcompany/web/module/pay/ExpiryDateCalculatorTest.java
+++ src/test/java/catchcompany/web/module/pay/ExpiryDateCalculatorTest.java
@@ -10,11 +10,22 @@ public class ExpiryDateCalculatorTest {
 
 	@Test
 	void 만원_납부시_한달_뒤가_만료일이_됨() {
-		assertExpiryDate(PayData.builder()
+		assertExpiryDate(
+			PayData.builder()
 				.billingDate(LocalDate.of(2019, 3, 1))
 				.payAmount(10_000)
 				.build(),
 			LocalDate.of(2019, 4, 1));
+	}
+
+	@Test
+	void 납부일의_한달_뒤_일자가_다를경우() {
+		assertExpiryDate(
+			PayData.builder()
+				.billingDate(LocalDate.of(2019, 1, 31))
+				.payAmount(10_000)
+				.build(),
+			LocalDate.of(2019, 2, 28));
 	}
 
 	private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate) {
