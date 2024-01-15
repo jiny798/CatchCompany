a81fef52f51ce5840fa41e48f3d725f4e68fe06f
jyoung7988 <jyoung7988@naver.com>
2024-01-15 12:56:45 +0900

    test: 10만원 납부 시, 1년 제공 테스트 추가

--- src/test/java/catchcompany/web/module/pay/ExpiryDateCalculator.java
+++ src/test/java/catchcompany/web/module/pay/ExpiryDateCalculator.java
@@ -6,7 +6,7 @@ import java.time.YearMonth;
 public class ExpiryDateCalculator {
 
 	public LocalDate calculateExpiryDate(PayData payData) {
-		int addedMonths = payData.getPayAmount() / 10_000;
+		int addedMonths = payData.getPayAmount() == 100_000 ? 12 : payData.getPayAmount() / 10_000;
 
 		if (payData.getFirstBillingDate() != null) { // 첫 납부일 존재할 경우
 			LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
