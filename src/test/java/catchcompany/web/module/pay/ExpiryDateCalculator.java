723217042693643cccf51e961c53943a3959cf92
jyoung7988 <jyoung7988@naver.com>
2024-01-15 12:50:18 +0900

    refactor: calculator 코드 리팩토링

--- src/test/java/catchcompany/web/module/pay/ExpiryDateCalculator.java
+++ src/test/java/catchcompany/web/module/pay/ExpiryDateCalculator.java
@@ -1,18 +1,28 @@
 package catchcompany.web.module.pay;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
 
 	public LocalDate calculateExpiryDate(PayData payData) {
 		int addedMonths = payData.getPayAmount() / 10_000;
-		if (payData.getFirstBillingDate() != null) {
+
+		if (payData.getFirstBillingDate() != null) { // 첫 납부일 존재할 경우
 			LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-			if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
-				return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
+			final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+			if (dayOfFirstBilling != candidateExp.getDayOfMonth()) {
+				final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
+				if (dayLenOfCandiMon < dayOfFirstBilling) {
+					return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+				}
+				return candidateExp.withDayOfMonth(dayOfFirstBilling);
+			} else {
+				return candidateExp;
 			}
+		} else { // 첫 납부일 존재하지 않을 때
+			return payData.getBillingDate().plusMonths(addedMonths);
 		}
 
-		return payData.getBillingDate().plusMonths(addedMonths);
 	}
 }
