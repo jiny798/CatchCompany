f590a4f231f012842ca359a1d5e105eddbe33c69
jyoung7988 <jyoung7988@naver.com>
2024-01-23 17:47:57 +0900

    refactor: static resource 에 대한 인증 무시

--- src/main/java/catchcompany/web/module/security/SecurityConfig.java
+++ src/main/java/catchcompany/web/module/security/SecurityConfig.java
@@ -2,12 +2,16 @@ package catchcompany.web.module.security;
 
 import static org.springframework.security.config.Customizer.*;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,26 +25,29 @@ import jakarta.servlet.DispatcherType;
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introSpector) throws Exception {
+	public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introSpector) throws
+		Exception {
 		http
-
-
 			.authorizeHttpRequests((req) -> req
-				.requestMatchers(new MvcRequestMatcher(introSpector,"/admin/**")).permitAll()
+				.requestMatchers(new MvcRequestMatcher(introSpector, "/admin/**")).permitAll()
 				.anyRequest().permitAll()
 			)
-			.formLogin(login -> login    // form 방식 로그인 사용
-				.defaultSuccessUrl("/", true)    // 성공 시 dashboard로
-				.loginProcessingUrl("/login")    // [B] submit 받을 url
-				.usernameParameter("id")    // [C] submit할 아이디
-				.passwordParameter("password")    // [D] submit할 비밀번호
+			.formLogin(login -> login
+				.defaultSuccessUrl("/", true)
+				.loginProcessingUrl("/login")
+				.usernameParameter("id")
+				.passwordParameter("password")
 				.permitAll()
 			)
 			.logout(withDefaults())
 			.csrf(csrf -> csrf.disable());
 
-		// https://docs.spring.io/spring-security/reference/features/exploits/headers.html#headers-xss-protection
 		return http.build();
+	}
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations()));
 	}
 
 	@Bean
